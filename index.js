0661ea73c486bee53528052766ae6f1d99fe3d3f
khaledawaza <1khaledawaza@gmail.com>
2022-10-14 07:22:32 +0200

    add additional two functions to my Users schema

--- index.js
+++ index.js
@@ -241,6 +241,22 @@ app.get("/directors/:director", (req, res) => {
     res.status(400);
   }
 });
+let userSchema = mongoose.Schema({
+  Username: {type: String, required: true},
+  Password: {type: String, required: true},
+  Email: {type: String, required: true},
+  Birthday: Date,
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+});
+
+userSchema.statics.hashPassword = (password) => {
+  return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compareSync(password, this.Password);
+}; 
+
 
 app.post("/users", (req, res) => {
   const newUser = req.body;
