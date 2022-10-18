0cf0af94a8526ba97378114498893db74c2b8ec7
khaledawaza <1khaledawaza@gmail.com>
2022-10-18 12:26:43 +0200

    add a library to my package

--- index.js
+++ index.js
@@ -104,28 +104,32 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something broke!");
 });
-
-app.post("/users", (req, res) => {
-  Users.findOne({ Username: req.body.Username })
+app.post('/users', (req, res) => {
+  let hashedPassword = Users.hashPassword(req.body.Password);
+  Users.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
     .then((user) => {
       if (user) {
-        return res.status(400).send(req.body.Username + "already exists");
+      //If the user is found, send a response that it already exists
+        return res.status(400).send(req.body.Username + ' already exists');
       } else {
-       
-        Users.create({
+        Users
+          .create({
             Username: req.body.Username,
-            Password: req.body.Password,
+            Password: hashedPassword,
             Email: req.body.Email,
             Birthday: req.body.Birthday
           })
-          .then((user) =>{res.status(201).json(user) })
-
-        .catch((error) => {
-          console.error(error);
-          res.status(500).send("Error: " + error);
-        })
+          .then((user) => { res.status(201).json(user) })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+          });
       }
     })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
 });
 
 // Add a movie to a user"s list of favorites
--- passport.js
+++ passport.js
@@ -7,28 +7,31 @@ const  passportJWT = require('passport-jwt');
 let Users = Models.User,
   JWTStrategy = passportJWT.Strategy,
   ExtractJWT = passportJWT.ExtractJwt;
-
-passport.use(new LocalStrategy({
-  usernameField: 'Username',
-  passwordField: 'Password'
-}, (username, password, callback) => {
-  console.log(username + '  ' + password);
-  console.error("users is "+username)
-  Users.findOne({ Username: username }, (error, user) => {
-    if (error) {
-      console.log(error);
-      return callback(error);
-    }
-
-    if (!user) {
-      console.log('incorrect username');
-      return callback(null, false, {message: 'Incorrect username or password.'});
-    }
-
-    console.log('finished');
-    return callback(null, user);
-  });
-}));
+  passport.use(new LocalStrategy({
+    usernameField: 'Username',
+    passwordField: 'Password'
+  }, (username, password, callback) => {
+    console.log(username + '  ' + password);
+    Users.findOne({ Username: username }, (error, user) => {
+      if (error) {
+        console.log(error);
+        return callback(error);
+      }
+  
+      if (!user) {
+        console.log('incorrect username');
+        return callback(null, false, {message: 'Incorrect username.'});
+      }
+  
+      if (!user.validatePassword(password)) {
+        console.log('incorrect password');
+        return callback(null, false, {message: 'Incorrect password.'});
+      }
+  
+      console.log('finished');
+      return callback(null, user);
+    });
+  }));
 
 passport.use(new JWTStrategy({
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
