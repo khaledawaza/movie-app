2a415b7b4e01d0243a75588c79d1ca0f9ab184b1
khaledawaza <1khaledawaza@gmail.com>
2022-10-31 11:46:49 +0100

    update auth file and point connectio uri to mongo db atlas

--- index.js
+++ index.js
@@ -41,13 +41,15 @@ mongoose.connect("mongodb://localhost:27017/myFlixDB", {
   
 
 
- mongoose.connect('mongodb+srv://admin:6Itu9nmTMeOhblBK@cluster0.vg1x4ip.mongodb.net/?retryWrites=true&w=majority' , { 
+ /* mongoose.connect('mongodb+srv://admin:6Itu9nmTMeOhblBK@cluster0.vg1x4ip.mongodb.net/?retryWrites=true&w=majority' , { 
   useNewUrlParser: true, 
   useUnifiedTopology: true,
- });
+ }); */
 
+ //const CONNECTION_URI = 'mongodb+srv://admin:6Itu9nmTMeOhblBK@cluster0.vg1x4ip.mongodb.net/?retryWrites=true&w=majority'
+//const CONNECTION_URI = 'mongodb://localhost:27017/myFlixDB'
  
- // mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
  let auth = require('./auth')(app);
 
@@ -312,21 +314,6 @@ app.get("/directors/:director", (req, res) => {
     res.status(400);
   }
 });
-let userSchema = mongoose.Schema({
-  Username: {type: String, required: true},
-  Password: {type: String, required: true},
-  Email: {type: String, required: true},
-  Birthday: Date,
-  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
-});
-
-userSchema.statics.hashPassword = (password) => {
-  return bcrypt.hashSync(password, 10);
-};
-
-userSchema.methods.validatePassword = function(password) {
-  return bcrypt.compareSync(password, this.Password);
-}; 
 
 
 app.post("/users", (req, res) => {
